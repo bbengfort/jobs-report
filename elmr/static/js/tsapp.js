0540e86954cd39a9f0aafe0acd95cc6225627da8
Benjamin Bengfort <benjamin@bengfort.com>
2015-05-04 12:03:26 -0400

    upper and lower add series

--- elmr/static/js/tsapp.js
+++ elmr/static/js/tsapp.js
@@ -1,61 +1,109 @@
-$(function() {
+/*
+ * Implements the code for handling a multi-timeseries graph
+ */
+function SeriesView() {
 
-  /*************************************************************************
-  ** Initialization
-  *************************************************************************/
+  // DOM Elements
+  this.elem = null;
+  this.svg  = null;
+  this.selector = null;
 
-  var endpoint  = "http://127.0.0.1:5000/api/series/LAUST280000000000005/";
-  var element   = $("#upper-series-view");
-  var dateFmt = "MMM YYYY";
+  // The series needs to be a list of objects
+  // Where each item has a period key defining the Month Year
+  // And every subsequent key is the value for a particular time series
+  this.series = [];
 
-  var margin = {top: 20, right: 80, bottom: 30, left: 60},
-      width = element.width() - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  // Defines the data fetch period and the number of elements in the series
+  this.start_year  = 2000;
+  this.end_year    = 2015;
+  this.date_format = "MMM YYYY";
 
-  var x = d3.time.scale()
-    .range([0, width]);
+  // Base endpoint for the timeseries API
+  this.base_url   = '/api/series/';
 
-  var y = d3.scale.linear()
-    .range([height, 0]);
+  // Other properties
+  this.margin = {top: 20, right: 80, bottom: 30, left: 60};
 
-  var color = d3.scale.category10();
+  // Graph properties
+  this.x = null;
+  this.y = null;
+  this.xAxis = null;
+  this.yAxis = null;
+  this.line  = null;
+  this.color = d3.scale.category10();
 
-  var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+  // Pass in a selector to initialize the view
+  this.init = function(selector) {
+    var self = this;
+    this.selector = selector;
+    this.elem = $(selector);
 
-  var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+    this.svg = d3.select(this.selector).append("svg")
+        .attr("width", this.width())
+        .attr("height", this.height())
+      .append("g")
+        .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");;
 
-  var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d) { return x(d.period); })
-    .y(function(d) { return y(d.value); });
+    this.x = d3.time.scale()
+      .range([0, this.width(true)]);
 
-  var svg = d3.select("#upper-series-view").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");;
+    this.y = d3.scale.linear()
+      .range([this.height(true), 0]);
 
-  d3.json(endpoint, function(error, data) {
-    data.data.forEach(function(d) {
-      d.period = moment(d.period, dateFmt);
+    this.xAxis = d3.svg.axis()
+      .scale(this.x)
+      .orient("bottom");
+
+    this.yAxis = d3.svg.axis()
+      .scale(this.y)
+      .orient("left");
+
+    this.line = d3.svg.line()
+      .interpolate("basis")
+      .x(function(d) { return self.x(d.period); })
+      .y(function(d) { return self.y(d.value); });
+
+    return this;
+  }
+
+  // Given a BLSID, fetch the data and draw the series
+  this.fetch_series = function(blsid) {
+    var self = this;
+    var endpoint = this.base_url + blsid;
+    endpoint += "?start_year=" + this.start_year + "&end_year=" + this.end_year;
+
+    d3.json(endpoint, function(error, response) {
+      // The JSON response contains some external info like source, title, etc.
+      // The series information is in a property called `data`, which contains
+      // objects that have period and value properties to add to the main series.
+      var data = response.data;
+
+      data.forEach(function(d) {
+        d.period = self.parse_date(d.period); // parse the date
+      });
+
+      // Temporary - we'll have to add the series to the view next.
+      self.series = data;
+      self.draw();
+
     });
 
+  }
 
-    x.domain(d3.extent(data.data, function(d) { return d.period; }));
-    y.domain(d3.extent(data.data, function(d) { return d.value; }));
+  // Draw the time series into the element
+  this.draw = function() {
 
-    svg.append("g")
+    this.x.domain(d3.extent(this.series, function(d) { return d.period; }));
+    this.y.domain(d3.extent(this.series, function(d) { return d.value; }));
+
+    this.svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .attr("transform", "translate(0," + this.height(true) + ")")
+        .call(this.xAxis);
 
-    svg.append("g")
+    this.svg.append("g")
         .attr("class", "y axis")
-        .call(yAxis)
+        .call(this.yAxis)
       .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
@@ -63,19 +111,59 @@ $(function() {
         .style("text-anchor", "end")
         .text("Value");
 
-    svg.append("path")
-        .datum(data.data)
+    this.svg.append("path")
+        .datum(this.series)
         .attr("class", "line")
-        .attr("d", line);
-  });
+        .attr("d", this.line);
 
-  var zoom = d3.behavior.zoom()
-    .x(x)
-    .y(y)
-    .scaleExtent([1, 10])
-    .on("zoom", zoomed);
-	
+  }
 
+
+  // Helper function to parse a date string (uses moment not d3)
+  this.parse_date = function(dtstr) {
+    return moment(dtstr, this.date_format);
+  }
+
+  // Helper functions to get the width from the element
+  // If the inner argument is true, the margins are subtracted
+  this.width = function(inner) {
+    if (inner) {
+      return this.elem.width() - this.margin.left - this.margin.right;
+    } else {
+      return this.elem.width();
+    }
+
+  }
+
+  // Helper functions to get the height from the element
+  // If the inner argument is true, the margins are subtracted
+  this.height = function(inner) {
+    if (inner) {
+      return this.elem.height() - this.margin.top - this.margin.bottom;
+    } else {
+      return this.elem.height();
+    }
+
+  }
+
+}
+
+
+var upper = new SeriesView().init("#upper-series-view");
+var lower = new SeriesView().init("#lower-series-view");
+
+$(function() {
+
+  /*************************************************************************
+  ** Initialization
+  *************************************************************************/
+
+
+
+
+
+  upper.fetch_series("LAUST280000000000005");
+  lower.fetch_series("SMS28000005500000001");
 
   console.log("Time Series Application Started");
 
